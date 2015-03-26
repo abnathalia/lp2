cbd2c8568853fa32caf1efab0c41917567cc4e9e
Nathália Andrade Barbosa <nathaliatp10@hotmail.com>
2015-03-26 14:23:16 -0300

    Create Exponencial.java

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,11 @@
+public class Exponencial{
+
+  public static void main (String [] agrs){
+    System.out.println("#\tQ\tC");
+    for (int i = 0; i <= 10; i++){
+      System.out.printf("%d\t%d\t%d%n", i, i*i, i*i*i);
+    }
+
+
+  }
+}
